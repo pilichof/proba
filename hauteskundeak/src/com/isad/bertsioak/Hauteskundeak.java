13a875382d5486fb00030cf86b4ca0b11976956f
Zihara <araizaloidab@gmail.com>
2016-09-28 09:54:08 +0200

    prueba2 karpeta ezabatu eta hauteskundeetan mahai berri bat gehitu, nire izenarekin

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -24,7 +24,7 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Irun");
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
-		
+		h2012.gehituMahaia("Zihara Badiola");
 		System.out.println(h2012);
 	}
 
